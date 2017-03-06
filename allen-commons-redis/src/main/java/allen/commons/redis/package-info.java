cf57ad14e0ecbdfa7a18bc1238098ef0c8544dc1
unknown <chenfei1@7F-Chenfei1.globale.com>
2017-03-06 10:38:42 +0800

    first commit

---
+++ allen-commons-redis/src/main/java/allen/commons/redis/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+/**
+ *
+ * All rights Reserved, Designed By HQYG
+ * Copyright:   Copyright(C) 2016
+ * Company:     HQYG.
+ * author:      cf
+ * Createdate:  2017年3月6日上午10:34:23
+ */
+package allen.commons.redis;
