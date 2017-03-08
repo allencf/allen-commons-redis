f81c478989ebde0fa19601872fe26e4830ab2cc6
allen <allen@7F-allen.globale.com>
2017-03-08 15:23:44 +0800

    提交测试代码…………

---
+++ allen-commons-redis/src/main/java/allen/commons/redis/config/RedisConfig.java
@@ -0,0 +1,23 @@
+package allen.commons.redis.config;
+
+import java.util.Properties;
+
+public class RedisConfig {
+
+	
+	public void init(){
+		
+	}
+	
+	
+	public void loadFileResource(){
+		Properties pro = new Properties();
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
